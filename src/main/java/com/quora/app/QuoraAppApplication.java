de0da9083cd861728c4d3e58e4010c676552fc2f
Govinth <govinth277@gmail.com>
2025-08-17 01:55:13 +0530

    env file add for dynamic value

--- src/main/java/com/quora/app/QuoraAppApplication.java
+++ src/main/java/com/quora/app/QuoraAppApplication.java
@@ -1,5 +1,6 @@
 package com.quora.app;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuoraAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuoraAppApplication.class, args);
+
+		Dotenv dotenv = Dotenv.configure().load(); // Load environment variables from .env file
+
+		dotenv.entries().
+				forEach(entry -> System.setProperty(entry.getKey(), entry.getValue())); // Set system properties from environment variables and set them
+		SpringApplication.run(QuoraAppApplication.class, args
+
+		);
 	}
 
 }
